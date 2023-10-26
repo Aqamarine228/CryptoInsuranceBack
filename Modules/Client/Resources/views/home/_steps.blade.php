91654b02748870bd3d1db0637fbb6b775b736d32
Aqamarine228 <MrFurgJK@gmail.com>
2023-10-26 14:26:33 +0200

    added new block to landing page

--- Modules/Client/Resources/views/home/_steps.blade.php
+++ Modules/Client/Resources/views/home/_steps.blade.php
@@ -1,51 +1,79 @@
-<div
-    class="elementor-element elementor-element-e533c1a elementor-widget elementor-widget-insur-icon-box"
-    style="margin-left: 30vw; margin-right: 30vw"
-    data-id="e533c1a"
-    data-element_type="widget"
-    data-widget_type="insur-icon-box.default"
->
-    <div class="elementor-widget-container">
-        <div class="make-a-claim__procedure-box">
-            <div class="make-a-claim__procedure-shape-1">
-                <img decoding="async"
-                     src="http://layerdrops.com/insurwp/wp-content/uploads/2023/03/make-a-claim-procedure-shape-1.png"
-                     alt="alt">
+<section class="process">
+    <div class="container">
+        <div class="section-title text-center">
+            <div class="section-sub-title-box">
+                <p class="section-sub-title">{{__('home.stepsTitle')}}</p>
+                <div class="section-title-shape-1">
+                    <img src="{{Module::asset('client:images/shapes/section-title-shape-1.png')}}" alt="">
+                </div>
+                <div class="section-title-shape-2">
+                    <img src="{{Module::asset('client:images/shapes/section-title-shape-2.png')}}" alt="">
+                </div>
             </div>
-
-            <ul class="make-a-claim__procedure list-unstyled ml-0">
-                <li>
-                    <div class="make-a-claim__procedure-icon-box">
-                        <div class="make-a-claim__procedure-icon insur-icon-svg">
-                            <span aria-hidden="true" class="  insur-three-icon-documents"></span></div>
-                        <div class="make-a-claim__procedure-icon-count"></div>
+            <h2 class="section-title__title">{{__('home.stepsSubTitle')}}</h2>
+        </div>
+        <div class="process__inner">
+            <div class="process-shape-1">
+                <img src="{{Module::asset('client:images/shapes/process-shape-1.png')}}" alt="">
+            </div>
+            <div class="row">
+                <div class="col-xl-3 col-lg-3 col-md-6">
+                    <div class="process__single">
+                        <div class="process__icon-box">
+                            <div class="process__icon">
+                                <span class="icon-select"></span>
+                            </div>
+                            <div class="process__count"></div>
+                        </div>
+                        <div class="process__content">
+                            <h3 class="process__title">{{__('home.step1Title')}}</h3>
+                            <p class="process__text">{{__('home.step1Description')}}</p>
+                        </div>
                     </div>
-                    <div class="make-a-claim__procedure-content">
-                        <h4 class="make-a-claim__procedure-title">Original policy <br> document</h4>
+                </div>
+                <div class="col-xl-3 col-lg-3 col-md-6">
+                    <div class="process__single process__single-2">
+                        <div class="process__icon-box">
+                            <div class="process__icon">
+                                <span class="icon-insurance-2"></span>
+                            </div>
+                            <div class="process__count"></div>
+                        </div>
+                        <div class="process__content">
+                            <h3 class="process__title">{{__('home.step2Title')}}</h3>
+                            <p class="process__text">{{__('home.step2Description')}}</p>
+                        </div>
                     </div>
-                </li>
-                <li>
-                    <div class="make-a-claim__procedure-icon-box">
-                        <div class="make-a-claim__procedure-icon insur-icon-svg">
-                            <span aria-hidden="true" class="   insur-three-icon-id-card"></span></div>
-                        <div class="make-a-claim__procedure-icon-count"></div>
+                </div>
+                <div class="col-xl-3 col-lg-3 col-md-6">
+                    <div class="process__single process__single-3">
+                        <div class="process__icon-box">
+                            <div class="process__icon">
+                                <span class="icon-meeting"></span>
+                            </div>
+                            <div class="process__count"></div>
+                        </div>
+                        <div class="process__content">
+                            <h3 class="process__title">{{__('home.step3Title')}}</h3>
+                            <p class="process__text">{{__('home.step3Description')}}</p>
+                        </div>
                     </div>
-                    <div class="make-a-claim__procedure-content">
-                        <h4 class="make-a-claim__procedure-title">Copy of <br> Identity card</h4>
+                </div>
+                <div class="col-xl-3 col-lg-3 col-md-6">
+                    <div class="process__single process__single-4">
+                        <div class="process__icon-box">
+                            <div class="process__icon">
+                                <span class="icon-insurance-agent"></span>
+                            </div>
+                            <div class="process__count"></div>
+                        </div>
+                        <div class="process__content">
+                            <h3 class="process__title">{{__('home.step4Title')}}</h3>
+                            <p class="process__text">{{__('home.step4Description')}}</p>
+                        </div>
                     </div>
-                </li>
-                <li>
-                    <div class="make-a-claim__procedure-icon-box">
-                        <div class="make-a-claim__procedure-icon insur-icon-svg">
-                            <span aria-hidden="true" class="   insur-three-icon-referral-code"></span></div>
-                        <div class="make-a-claim__procedure-icon-count"></div>
-                    </div>
-                    <div class="make-a-claim__procedure-content">
-                        <h4 class="make-a-claim__procedure-title">Maturity discharge <br> voucher</h4>
-                    </div>
-                </li>
-            </ul>
-
+                </div>
+            </div>
         </div>
     </div>
-</div>
+</section>
